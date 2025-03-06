ae962ade5e56ba841bfdd588beff9b2b7cd06ccd
Alex Severin <severinalexeyv@gmail.com>
2025-03-06 14:16:08 +0300

    isort

--- packages/common/src/common/stt/__init__.py
+++ packages/common/src/common/stt/__init__.py
@@ -2,7 +2,6 @@ from .model import SpeechToTextModel
 from .schemas import Segment, SpeechToTextOptions, SpeechToTextResult, SpeechToTextTask
 from .whisper_schemas import WhisperCfg, WhisperDevice, WhisperSize
 
-
 __all__ = [
     "Segment",
     "SpeechToTextModel",
