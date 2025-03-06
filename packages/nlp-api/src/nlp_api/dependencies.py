c0bc2bf0b33b4f009eeeae24e000cf9cbf2df25d
Alex Severin <severinalexeyv@gmail.com>
2025-03-06 16:16:03 +0300

    mypy

--- packages/nlp-api/src/nlp_api/dependencies.py
+++ packages/nlp-api/src/nlp_api/dependencies.py
@@ -4,8 +4,8 @@ from .state import ApiState
 
 
 def get_app(request: Request) -> FastAPI:
-    return request.app
+    return request.app  # type: ignore
 
 
 def get_api_state(app: FastAPI = Depends(get_app)) -> ApiState:
-    return app.state.api_state
+    return app.state.api_state  # type: ignore
