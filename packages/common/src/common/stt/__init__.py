b1d152292b5cfa92440e342d3e34d3262bfb9eaf
Alex Severin <severinalexeyv@gmail.com>
2025-03-06 14:15:30 +0300

    add stt __all__

--- packages/common/src/common/stt/__init__.py
+++ packages/common/src/common/stt/__init__.py
@@ -1,3 +1,15 @@
 from .model import SpeechToTextModel
-from .schemas import Segment, SpeechToTextTask, SpeechToTextResult, SpeechToTextOptions
-from .whisper_schemas import WhisperCfg, WhisperSize, WhisperDevice
+from .schemas import Segment, SpeechToTextOptions, SpeechToTextResult, SpeechToTextTask
+from .whisper_schemas import WhisperCfg, WhisperDevice, WhisperSize
+
+
+__all__ = [
+    "Segment",
+    "SpeechToTextModel",
+    "SpeechToTextOptions",
+    "SpeechToTextResult",
+    "SpeechToTextTask",
+    "WhisperCfg",
+    "WhisperDevice",
+    "WhisperSize",
+]
