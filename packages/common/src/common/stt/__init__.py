df368ea470615201f8f256864559747f7954b108
Alex Severin <severinalexeyv@gmail.com>
2025-03-06 14:13:15 +0300

    add test for model from_whisper_cfg

---
+++ packages/common/src/common/stt/__init__.py
@@ -0,0 +1,3 @@
+from .model import SpeechToTextModel
+from .schemas import Segment, SpeechToTextTask, SpeechToTextResult, SpeechToTextOptions
+from .whisper_schemas import WhisperCfg, WhisperSize, WhisperDevice
